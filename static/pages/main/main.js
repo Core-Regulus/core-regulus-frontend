2ae23f24727248754ccbef87e19401473c6319c1
Shteinaseev <142624493+Shteinaseev@users.noreply.github.com>
2025-06-13 13:40:46 +0200

    Update main.js

--- static/pages/main/main.js
+++ static/pages/main/main.js
@@ -7,4 +7,27 @@ export class CorePage extends Page {
 
 }
 
+const headerEl = document.querySelector(".header")
+
+window.addEventListener("scroll", function(){
+    const scrollPos = window.scrollY
+
+    if(scrollPos > 100) {
+        headerEl.classList.add("header__scroll")
+    }else{
+        headerEl.classList.remove("header__scroll")
+    }
+})
+
+const menuIcon = document.querySelector('#menu-icon')
+const navbar = document.querySelector('.navbar')
+const navbg = document.querySelector('.nav-bg')
+
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+    navbg.classList.toggle('active')
+} )
+
 customElements.define('core-page', CorePage);
